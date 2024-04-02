393d7c45cea92c712d2893419abca4bfafe79f4a
Sonu Kumar <155009224+coder-sonu@users.noreply.github.com>
2024-04-02 14:31:31 +0000

    container config done

--- firstclass/test.js
+++ firstclass/test.js
@@ -1,1 +1,11 @@
-console.log("hello World");
+// console.log("hello World");
+
+const accountid = 123456;
+let accountname = "sonu@gmail.com";
+var accountpassword = 12385;
+accountcity = "bihar";
+
+console.log(accountcity);
+// console.log(accountid,accountname,accountpassword);
+
+// console.table([accountid,accountname,accountpassword]);
